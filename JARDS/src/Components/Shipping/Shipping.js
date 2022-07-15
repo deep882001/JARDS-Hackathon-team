a14e945a04e6f939949255bf0bf840dbc6818059
Agualpa <108818271+Agualpa@users.noreply.github.com>
2022-07-14 20:37:34 -0400

    Add files via upload

---
+++ JARDS/src/Components/Shipping/Shipping.js
@@ -0,0 +1,154 @@
+import React from 'react'
+import'./Shipping.css'
+
+function Shipping() {
+  return (
+    <div>
+           <div class="background">
+        <div class="container">
+            <div class="panel chart">
+ 
+
+  <body>
+       <img
+        src="https://logos-download.com/wp-content/uploads/2016/08/UPS_logo_logotype.png"
+         alt=""
+         width="150"
+         height="150"
+        />
+      <h1 id="title">UPS Shipping Estimate Chart</h1>
+      <h2> <font size ="+3"> Flat Rate Shipping - UPS Simple Rate </font></h2>
+      
+      <div class="pricing-plan">
+          <h2 class="header">EXTRA SMALL</h2>
+          <img src="https://raw.githubusercontent.com/deep882001/JARDS-Hackathon-team/main/JARDS/IMG_4387.JPG" alt=" "class = "Photo"></img> 
+          <ul class="features">
+                <li class="item">Starting at $9.46</li>
+                <li class="item">4 in x 4 in x 4 in</li>
+                <li class="item">Sunglasses, t-shirt, ceramic mug</li>
+                </ul>
+                <a href="#/" class="button">SELECT</a>
+                </div>
+
+        <div class="pricing-plan">
+                
+                <h2 class="header">SMALL</h2>
+                <img src="https://raw.githubusercontent.com/deep882001/JARDS-Hackathon-team/main/JARDS/IMG_4383.JPG" alt=" " class = "Photo"></img>
+                <ul class="features">
+                <li class="item">Starting at $12.40</li>
+                <li class="item">6 in x 6 in x 6 in, 8 in x 6 in x 5 in, 12 in x 9 in x 2 in</li>
+                <li class="item">Hardcover book, tissue box or ream of paper</li>
+                </ul>
+                <a href="#/" class="button">SELECT</a>
+            </div>
+
+            <div class="pricing-plan">
+                
+                <h2 class="header">MEDIUM</h2>
+                <img src="https://raw.githubusercontent.com/deep882001/JARDS-Hackathon-team/main/JARDS/medium.JPG" alt=" "class = "Photo"></img>
+                <ul class="features">
+                <li class="item">Starting at $14.95</li>
+                <li class="item">8 in x 8 in x 8 in, 12 in x 9 in x 6 in, 13 in x 11 in x 2 in</li>
+                <li class="item">Shoebox or three-inch, three-ring binder</li>
+                </ul>
+                <a href="#/" class="button">SELECT</a>
+            </div>
+
+
+            <div class="pricing-plan">
+                
+                <h2 class="header">LARGE</h2>
+                <img src="https://raw.githubusercontent.com/deep882001/JARDS-Hackathon-team/main/JARDS/large.JPG" alt=" "class = "Photo"></img>
+                <ul class="features">
+                <li class="item">Starting at $14.95</li>
+                <li class="item">10 in x 10 in x 10 in, 12 in x 12 in x 7 in, 15 in x 11 in x 6 in</li>
+                <li class="item">Regulation basketball or 14-inch laptop</li>
+                </ul>
+                <a href="#/" class="button">SELECT</a>
+            </div>
+
+
+            <div class="container1">
+            <p>UPS Simple Rate Advantages </p>  
+            <img src="https://raw.githubusercontent.com/deep882001/JARDS-Hackathon-team/main/JARDS/line.png" alt=" "class = "Photo2"></img>
+            </div>
+            <table class="table table-bordered text-center w-0">
+            <tr>
+            <th colspan="10" class = "NAME">How UPS Simple Rate Compares </th>
+            </tr>
+
+         <tbody>      
+           
+                <tr class="success">
+                  <th>Feature </th>
+                  <th> UPS </th>
+                  <th> FedEx </th>
+                  <th> USPS </th>
+                </tr>
+              </tbody>
+
+        <tr class="table-success">
+          <td>Customer Packaging  </td>
+          <td> YES  </td>
+          <td> ---------  </td>
+          <td> ----------</td>
+        </tr>
+
+        <tr class="success">
+    
+          <td>Maximum Volume</td>
+          <td>Starting at $12.40</td>
+          <td>12:30pm</td>
+          <td>1:45pm</td>
+        </tr>
+
+        <tr class="table-success">
+          <td>Medium</td>
+          <td>Up to 1,728 cubic inches</td>
+          <td>Up to 1,452 cubic inches</td>
+          <td>Up to 1200 cubic inches</td>
+        </tr>
+        
+        <tr class="success">
+            <td>Shipping Speeds</td>
+            <td>Within Five Days, Within Three Business Days, Second Day and Next-Day</td>
+            <td>Express Services only (no ground)</td>
+            <td>Priority Mail 1-3 budiness days</td>
+          </tr>
+        
+          <tr class="table-success">
+            <td>Guaranteed</td>
+            <td>  YES </td>
+            <td>  YES  </td>
+            <td> ---------- </td>
+        </tr>
+
+        <tr class="success">
+            <td> Shipping Rates </td>
+            <td>  National Flat Rate </td>
+            <td>  3 Zones </td>
+            <td> National Flat Rate </td>
+        </tr>
+      </table>
+     
+    </body>
+
+    <div>
+
+      
+        
+            </div>
+        </div>
+        </div>
+        </div>
+  
+    </div>
+   
+  )
+}
+
+
+
+
+
+export default Shipping
